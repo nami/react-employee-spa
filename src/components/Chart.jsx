53302da55338c3aa046c14fea0f720ad09804125
nami izarra <nami.izarra@gmail.com>
2021-03-29 23:33:17 +0900

    extract labels for bar chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import data from "../data/EmployeeDataset.json";
 import { Container, Grid, Paper, Typography } from "@material-ui/core";
+import { Bar } from "react-chartjs-2";
+
+const extractLabels = () => {
+  // get array of locations including duplicates
+  const allLocations = data.map((obj) => obj.location);
+  // retrieve only
+  return [...new Set(allLocations)];
+};
+
+const chartData = () => {};
 
 const Chart = (props) => {
-  return <div>Chart</div>;
+  return (
+    <Container maxWidth="lg" className="chartContainer">
+      <Grid container style={{ backgroundColor: "#F7F7F7" }}>
+        <Grid item xs={12}></Grid>
+        <div className="barChart">
+          {console.log(extractLabels())}
+          <Bar />
+        </div>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Chart;
