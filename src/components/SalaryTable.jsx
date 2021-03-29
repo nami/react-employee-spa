9a125375a0f89ca2a83aa02306d47eb13e304186
nami izarra <nami.izarra@gmail.com>
2021-03-30 02:18:00 +0900

    rename table

---
+++ src/components/SalaryTable.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { extractSalaries } from "../utils";
+import {
+  Container,
+  Grid,
+  Table,
+  TableBody,
+  TableHead,
+  TableRow,
+  TableContainer,
+  TableCell,
+} from "@material-ui/core";
+
+function createData(location, salary, delta) {
+  return { location, salary, delta };
+}
+
+const tableRows = extractSalaries().map((obj) =>
+  createData(obj.location, obj.salary, obj.delta)
+);
+
+const rows = [];
+
+const SalaryTable = (props) => {
+  return (
+    <Container maxWidth="lg" className="chartContainer">
+      <Grid container>
+        <Grid item xs={12}></Grid>
+        <div className="salaryTable">
+          {console.log(tableRows)}
+          <TableContainer>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Location</TableCell>
+                  <TableCell align="right">Salary</TableCell>
+                  <TableCell align="right">Delta</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {tableRows.map((row) => (
+                  <TableRow key={row.salary}>
+                    <TableCell component="th" scope="row">
+                      {row.location}
+                    </TableCell>
+                    <TableCell align="right">${row.salary}</TableCell>
+                    <TableCell align="right">{row.delta}%</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </Grid>
+    </Container>
+  );
+};
+
+export default SalaryTable;
--- src/components/Table.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import data from "../data/EmployeeDataset.json";
-import { Container, Grid, Paper, Typography } from "@material-ui/core";
-
-const Table = (props) => {
-  return <div>Table</div>;
-};
-
-export default Table;
