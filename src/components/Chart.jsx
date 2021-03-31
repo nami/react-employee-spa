9edef9f036d5e5f6146ec414d572e5e52c577ba1
nami izarra <nami.izarra@gmail.com>
2021-04-01 03:32:25 +0900

    filter by locations in chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,25 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { extractLabels, extractSalaries, formatAmount } from "../utils";
 import Checkboxes from "./Checkboxes";
 import { Container, Paper } from "@material-ui/core";
 import { Bar } from "react-chartjs-2";
 
-const chartData = {
-  labels: extractLabels(),
-  datasets: [
-    {
-      label: "Aggregated current salaries",
-      backgroundColor: "#04A49A",
-      // remove Total object from chart
-      data: extractSalaries()
-        .filter((obj) => obj.location !== "Total")
-        .map((obj) => obj.salary),
-    },
-  ],
-};
-
 const Chart = () => {
-  const [checkedLocation, setCheckedLocation] = useState();
+  const [checkedLocations, setCheckedLocations] = useState([]);
+  const [salaries, setSalaries] = useState(extractSalaries());
+  const [locationLabels, setLocationLabels] = useState(extractLabels());
+
+  const chartData = {
+    labels: locationLabels,
+    datasets: [
+      {
+        label: "Aggregated current salaries",
+        backgroundColor: "#04A49A",
+        // remove Total object from chart
+        data: salaries
+          .filter((obj) => obj.location !== "Total")
+          .map((obj) => obj.salary),
+      },
+    ],
+  };
+
+  useEffect(() => {
+    if (checkedLocations.length > 0) {
+      // salaries filtered by location/s
+      const newSalaries = extractSalaries().filter((obj) =>
+        checkedLocations.includes(obj.location)
+      );
+      setSalaries(newSalaries);
+      setLocationLabels(checkedLocations);
+    } else {
+      // show all salaries when no checkboxes are checked
+      setSalaries(extractSalaries());
+      setLocationLabels(extractLabels());
+    }
+  }, [checkedLocations]);
+
+  const handleLocation = (locations) => {
+    setCheckedLocations(locations);
+  };
 
   return (
     <div>
@@ -42,7 +63,10 @@ const Chart = () => {
           }}
         />
       </Container>
-      <Checkboxes />
+      <Checkboxes
+        handleLocation={handleLocation}
+        locations={checkedLocations}
+      />
     </div>
   );
 };
