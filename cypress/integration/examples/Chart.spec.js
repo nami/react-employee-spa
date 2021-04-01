8eb421fe82f0e906e8741a03a10adb4fcf4e89f1
nami izarra <nami.izarra@gmail.com>
2021-04-01 16:31:42 +0900

    implement cypress e2e for chart

---
+++ cypress/integration/examples/Chart.spec.js
@@ -0,0 +1,17 @@
+import { extractLabels } from "../../../src/utils";
+
+const labels = extractLabels();
+
+describe("Chart has data can be checked", () => {
+  it("can check checkboxes", () => {
+    cy.visit("localhost:3000");
+    // should have bar chart
+    cy.get("canvas.chartjs-render-monitor").should("exist");
+    // check if checkbox exists with label
+    cy.get(`input[name=${labels[0]}]`).should("exist");
+    cy.get("span").contains(`${labels[0]}`).should("exist");
+    // check checkbox
+    cy.get(`input[name=${labels[0]}]`).check();
+    cy.get(`input[name=${labels[0]}]`).should("be.checked");
+  });
+});
